3151580114790ba26d3d4bc31a56d85f50cd715b
John Trupiano <jtrupiano@gmail.com>
2009-10-12 21:44:10 -0400

    Allow regexp substitution for all rewrite rules.

--- lib/rack-rewrite/rule.rb
+++ lib/rack-rewrite/rule.rb
@@ -39,31 +39,35 @@ module Rack
       # Either (a) return a Rack response (short-circuting the Rack stack), or
       # (b) alter env as necessary and return true
       def apply!(env)
-        case rule_type
+        interpreted_to = self.send(:interpret_to, env['PATH_INFO'])
+        case self.rule_type
         when :r301
-          [301, {'Location' => self.to}, ['Redirecting...']]
+          [301, {'Location' => interpreted_to}, ['Redirecting...']]
         when :r302
-          [302, {'Location' => self.to}, ['Redirecting...']]
+          [302, {'Location' => interpreted_to}, ['Redirecting...']]
         when :rewrite
           # return [200, {}, {:content => env.inspect}]
-          env['PATH_INFO'] = env['REQUEST_URI'] = self.to
+          env['PATH_INFO'] = env['REQUEST_URI'] = interpreted_to
           true
         else
           raise Exception.new("Unsupported rule: #{rule.rule_type}")
         end
       end
-
-      def rewrite?
-        self.rule_type == :rewrite
-      end
-
-      def r301?
-        self.rule_type == :r301
-      end
-
-      def r302?
-        self.rule_type == :r302
-      end
+      
+      private
+        # is there a better way to do this?
+        def interpret_to(path)
+          if self.from.is_a?(Regexp)
+            if from_match_data = self.from.match(path)
+              computed_to = self.to.dup
+              (from_match_data.size - 1).downto(1) do |num|
+                computed_to.gsub!("$#{num}", from_match_data[num])
+              end
+              return computed_to
+            end
+          end
+          self.to
+        end
     end
     
   end
--- test/rule_test.rb
+++ test/rule_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class RuleTest < Test::Unit::TestCase
 
+  context '#Rule#apply' do
+    should 'set Location header to result of #interpret_to for a 301' do
+      rule = Rack::Rewrite::Rule.new(:r301, %r{/abc}, '/def')
+      env = {'PATH_INFO' => '/abc'}
+      assert_equal rule.send(:interpret_to, '/abc'), rule.apply!(env)[1]['Location']
+    end
+  end
+  
   context 'Rule#matches' do
     
     context 'Given any rule with a "from" string of /features' do
@@ -48,4 +56,29 @@ class RuleTest < Test::Unit::TestCase
       end
     end
   end
+  
+  context 'Rule#interpret_to' do
+    
+    should 'return #to when #from is a string' do
+      rule = Rack::Rewrite::Rule.new(:rewrite, '/abc', '/def')
+      assert_equal '/def', rule.send(:interpret_to, '/abc')
+    end
+    
+    should 'replace $1 on a match' do
+      rule = Rack::Rewrite::Rule.new(:rewrite, %r{/person_(\d+)}, '/people/$1')
+      assert_equal '/people/1', rule.send(:interpret_to, "/person_1")
+    end
+    
+    should 'be able to catch querystrings with a regexp match' do
+      rule = Rack::Rewrite::Rule.new(:rewrite, %r{/person_(\d+)(.*)}, '/people/$1$2')
+      assert_equal '/people/1?show_bio=1', rule.send(:interpret_to, '/person_1?show_bio=1')
+    end
+    
+    should 'be able to make 10 replacements' do
+      # regexp to reverse 10 characters
+      rule = Rack::Rewrite::Rule.new(:rewrite, %r{(\w)(\w)(\w)(\w)(\w)(\w)(\w)(\w)(\w)(\w)}, '$10$9$8$7$6$5$4$3$2$1')
+      assert_equal 'jihgfedcba', rule.send(:interpret_to, "abcdefghij")
+    end
+  end
+  
 end
